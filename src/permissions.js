leibniz-psychology/bawwab-client

commit 7db7d91e758e45349e5e5f9b88e9e5cce69e86eb

Allow re-sharing a project
While it was possible from a technical perspective, because every group
member can add new group members, the share button was hidden on shared
projects. While at it also refactor share permission handling, so we
can differentiate between read and write share permission.

src/permissions.js
@@ -27,12 +27,6 @@ export default class Permissions {
 		return this.s.includes ('T');
 	}
 
-	/* Current user can share project with other users.
-	 */
-	get canShare () {
-		return this.canWriteNamedAttributes;
-	}
-
 	/* Current user can delete files. In theory having 'w' is enough, but don’t
 	 * advertise it. */
 	get canDelete () {

src/view/workspaceList/index.js
@@ -25,7 +25,7 @@ export default {
 		username: function () { return this.state.user?.name; },
 		filteredWorkspaces: function () {
 			const filterFunc = {
-				mine: w => w.permissions.mine.canShare,
+				mine: w => w.permissions.mine.canWriteNamedAttributes,
 				shared: w => !w.permissions.mine.canWriteNamedAttributes && !w.isPublic,
 				world: w => w.isPublic,
 				};

src/view/workspaceList/template.html
@@ -70,7 +70,7 @@
 		</td>
 		<td class="wl-table_actions">
 			<ul class="wl-actionsList">
-				<li class="wl-actionItem" v-if="w.permissions.mine.canShare">
+				<li class="wl-actionItem" v-if="w.canShare">
 					<tooltip>
 						<router-link class="wl-actionItem_button btn small" :to="{name: 'workspaceShare', params: {wsid: w.metadata._id}}" aria-label="share"><i class="fas fa-share"></i></router-link>
 						<template v-slot:tip>{{ $t('v.workspaceList.share') }}</template>

src/view/workspaceShare/index.js
@@ -4,7 +4,6 @@ import { queryParamProp } from '../../utils.js';
 import template from './template.html';
 import { trackEvent } from "../../matomo.js";
 import { getResponse } from '../../helper';
-import { groupCreate } from '../../usermgr';
 import './style.css';
 
 const SendStatus = Object.freeze ({
@@ -56,65 +55,11 @@ export default {
 		},
 	},
 	created: async function() {
-		const ws = this.workspace;
-
-		async function createAction (group) {
-			console.debug (`Creating action for group ${group}`);
-			/* {user} is a special symbol, which will be resolved upon evaluation of the action */
-			const command = ['usermgr', 'g', 'add', group, '{user}'];
-			const r = await postData('/api/action', {
-					name: 'run',
-					extra: {path: ws.path},
-					command: command,
-					/* 100 years (not kidding) */
-					validFor: 100*365*24*60*60,
-					/* yes, also not kidding */
-					usesRemaining: 10**10,
-					});
-			const action = await getResponse (r);
-			return action.token;
-		}
-
-		/* Do the groups exist? */
-		let groups = {false: null, true: null};
-
-		for (const g in ws.permissions.acl.group) {
-			const p = ws.permissions.acl.group[g];
-			if (p.canRead && !p.canWrite) {
-				groups[false] = g;
-			} else if (p.canRead && p.canWrite) {
-				groups[true] = g;
-			}
-		}
-
-		async function createGroupForWorkspace (ws, isWrite) {
-			console.debug (`Creating group for workspace ${ws} ${isWrite}`);
-			const name = ws.path.split ('/').pop () + (isWrite ? '-rw' : '-ro');
-			return await groupCreate (name);
-		}
-
-		if (groups[false] === null) {
-			const newgroup = await createGroupForWorkspace (ws, false);
-			groups[false] = newgroup.group;
-			await this.workspaces.share (ws, `g:${groups[false]}`, false);
-		}
-		if (groups[true] === null) {
-			const newgroup = await createGroupForWorkspace (ws, true);
-			groups[true] = newgroup.group;
-			await this.workspaces.share (ws, `g:${groups[true]}`, true);
-		}
-
-		/* async resolve both actions */
-		const keys = [false, true];
-		const values = await Promise.all (keys.map (function (isWrite) {
-			return createAction (groups[isWrite]);
-		}.bind (this)));
-		for (let i = 0; i < keys.length; i++) {
-			const isWrite = keys[i];
-			const token = values[i];
-			const ident = 'share-' + (isWrite ? 'write' : 'readonly');
-			this.shareUrl[isWrite] = new URL (`/action/${token}#${ident}`, window.location.href);
-		}
+		const values = await Promise.all ([
+				this.workspaces.createShareUrl (this.workspace, false),
+				this.workspaces.createShareUrl (this.workspace, true)]);
+		this.shareUrl[false] = values[0];
+		this.shareUrl[true] = values[1];
 	},
 	methods: {
 		copyToClipboard: async function(text) {

src/view/workspaceShare/template.html
@@ -1,7 +1,7 @@
 <modal icon="users" :title="$t('v.workspaceShare.sharetitle')" :closeName="$t('v.workspaceShare.close')" :closeLink="{name: 'workspace', params: {wsid: workspace.metadata._id}}" :scaling="true" class="share-project">
 	<select v-model="selectedShareUrl" name="shareKind" size="0" class="wsh-kindSelector">
-		<option selected="selected" :value="false" class="read">{{ $t('v.workspaceShare.read') }}</option>
-		<option :value="true" class="write">{{ $t('v.workspaceShare.write') }}</option>
+		<option selected="selected" :disabled="!shareUrl[false]" :value="false" class="read">{{ $t('v.workspaceShare.read') }}</option>
+		<option :disabled="!shareUrl[true]" :value="true" class="write">{{ $t('v.workspaceShare.write') }}</option>
 	</select>
 	<span class="wsh-kindMeaning">{{ shareMeaning }}</span>
 

src/view/workspaceShow/template.html
@@ -53,7 +53,7 @@
 		<div class="ws-secondaryActions">
 			<router-link class="btn ws-actionButton" v-if="permissions.canRun" :to="{name: 'workspacePackages', params: {wsid: workspace.metadata._id}}"><i class="fas fa-box"></i> {{ $t('v.workspaceShow.editpackages') }}</router-link>
 			<router-link class="btn ws-actionButton" :to="{name: 'fileManager', params: {wsidA: workspace.metadata._id}}"><i class="fas fa-file"></i> {{ $t('v.workspaceShow.files') }}</router-link>
-			<router-link class="btn ws-actionButton" v-if="permissions.canShare" :to="{name: 'workspaceShare', params: {wsid: workspace.metadata._id}}"><i class="fas fa-share"></i> {{ $t('v.workspaceShow.share') }}</router-link>
+			<router-link class="btn ws-actionButton" v-if="workspace.canShare" :to="{name: 'workspaceShare', params: {wsid: workspace.metadata._id}}"><i class="fas fa-share"></i> {{ $t('v.workspaceShow.share') }}</router-link>
 			<router-link class="btn ws-actionButton" v-if="permissions.canShare" :to="{name: 'workspacePublish', params: {wsid: workspace.metadata._id}}"><i class="fas fa-globe"></i> {{ $t('v.workspaceShow.publish') }}</router-link>
 			<router-link class="btn ws-actionButton" v-if="permissions.canRead" :to="{name: 'workspaceExport', params: {wsid: workspace.metadata._id}}"><i class="fas fa-file-export"></i> {{ $t('v.workspaceShow.export') }}</router-link>
 			<action-button v-if="permissions.canRead" icon="copy" :f="copy" ref="copybtn" class="copy ws-actionButton">{{ $t('v.workspaceShow.copy') }}</action-button>

src/workspace.js
@@ -73,6 +73,21 @@ export default class Workspace {
 		return users;
 	}
 
+	/* User can share project read-only */
+	get canShareRead () {
+		return this.permissions.mine.canRead && !this.isPublic;
+	}
+
+	/* User can share project read-write */
+	get canShareWrite () {
+		return this.permissions.mine.canWrite && !this.isPublic;
+	}
+
+	/* User can share project (any mode) */
+	get canShare () {
+		return this.canShareRead || this.canShareWrite;
+	}
+
 	get id () {
 		return this.metadata._id;
 	}

src/workspaces.js
@@ -3,6 +3,7 @@ import { ConductorClient } from './conductor.js';
 import { publicData, privateData } from './config';
 import { postData, getResponse } from './helper.js';
 import { run as emrun, register as emregister } from './eventManager.js';
+import { groupCreate } from './usermgr.js';
 
 import { reactive } from 'vue/dist/vue.esm-bundler.js';
 
@@ -278,6 +279,53 @@ export default class Workspaces {
 	 */
 	add (ws) {
 		this.workspaces.set (ws.id, ws);
+	}
+
+	async createShareUrl (ws, isWrite) {
+		let group = null;
+
+		if ((!isWrite && !ws.canShareRead) || (isWrite && !ws.canShareWrite)) {
+			/* Project cannot be shared, so bail out early */
+			return null;
+		}
+
+		for (const g in ws.permissions.acl.group) {
+			const p = ws.permissions.acl.group[g];
+			if (!isWrite && p.canRead && !p.canWrite) {
+				group = g;
+				console.debug (`workspaces: Using group ${group} for read-only sharing`);
+			} else if (isWrite && p.canRead && p.canWrite) {
+				group = g;
+				console.debug (`workspaces: Using group ${group} for read-write sharing`);
+			}
+		}
+
+		if (!group) {
+			console.debug (`Creating group for workspace ${ws.id} ${isWrite}`);
+			const name = ws.path.split ('/').pop () + (isWrite ? '-rw' : '-ro');
+			const newgroup = await groupCreate (name);
+			group = newgroup.group;
+			await this.share (ws, `g:${group}`, isWrite);
+		}
+
+		console.debug (`Creating action for group ${group}`);
+		/* {user} is a special symbol, which will be resolved upon evaluation
+		 * of the action */
+		const command = ['usermgr', 'g', 'add', group, '{user}'];
+		const r = await postData('/api/action', {
+				name: 'run',
+				extra: {path: ws.path},
+				command: command,
+				/* 100 years (not kidding) */
+				validFor: 100*365*24*60*60,
+				/* yes, also not kidding */
+				usesRemaining: 10**10,
+				});
+		const action = await getResponse (r);
+		const token = action.token;
+
+		const ident = 'share-' + (isWrite ? 'write' : 'readonly');
+		return new URL (`/action/${token}#${ident}`, window.location.href);
 	}
 }
 

commit 3dfb16edccf25d04e48ddd1142c6af69ffa04420

Fix project deletion
Broken by commit 7db7d91e758e45349e5e5f9b88e9e5cce69e86eb, which left
canShare undefined, but used by canDelete.

src/permissions.js
@@ -30,6 +30,6 @@ export default class Permissions {
 	/* Current user can delete files. In theory having 'w' is enough, but don’t
 	 * advertise it. */
 	get canDelete () {
-		return this.canShare;
+		return this.canWriteNamedAttributes;
 	}
 }