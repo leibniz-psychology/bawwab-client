dab812c66bb9392377dde06eb2e5aff0dd699a75
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2023-03-20 11:24:52 +0100

    component: message: Unhide when content is changed.

--- src/component/message/index.js
+++ src/component/message/index.js
@@ -4,8 +4,24 @@ import './style.css';
 export default {
 	name: 'Message',
 	props: ['kind'],
-	data: function () { return {visible: true} },
+	data: function () { return {visible: true, observer: null} },
 	template: template,
+	mounted: function () {
+		/* There is no way to watch $slots for changes, so we have to add a
+		 * MutationObserver to the element containing the <slot> and watch for
+		 * changes inside. This ensures the message is shown again whenever
+		 * its content changes. */
+		const observer = this.observer = new MutationObserver (this.show);
+		observer.observe (this.$refs.content, {
+			attributes: true,
+			childList: true,
+			characterData: true,
+			subtree: true,
+		});
+	},
+	unmounted: function() {
+		this.observer.disconnect ();
+	},
 	computed: {
 		iconclass: function () {
 			const kindToIcon = {
@@ -22,5 +38,8 @@ export default {
 		hide: function () {
 			this.visible = false;
 		},
-	}
+		show: function() {
+			this.visible = true;
+		}
+	},
 };
